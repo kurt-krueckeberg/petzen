5ae92f9fe7caaaa05f6c0cb927db2a90cf0bbdbd
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2022-11-01 19:26:12 -0400

    u

--- main.php
+++ main.php
@@ -1,26 +1,29 @@
 <?php
 declare(strict_types=1);
+use GenTools\tNail;
+include 'boot-strap/boot-strap.php';
+boot_strap();
+
 /*
   argv[1] == start direcory
  */ 
 $start_dir = $argv[1]; 
+$3ext = 'jpg';
 
 $jpg_iter = new \CallbackFilterIterator(new DirectoryIterator($start_dir),
    function(\SplFileInfo $info) use ($ext) {  
       return $info->isfile()  && $info->getfilename() === $ext;
    });
 
-$t_maker =  new thumbGenerator(); 
+$t_maker =  new tNail(); 
 
 $functor = function(\SplFileInfo $info) use ($t_maker) 
 {
-   $base_name = $info->getBasename('.md');
+   $fname = $info->getFilename();
 
-   $output = $base_name . '.html'; 
+   $output = 't' . $fname; 
    
-   $t_maker->generate();
-   
-   fix_td_tags($base_name);
+   $t_maker->generate($fname, 400, 400, $output);
 };
 
 foreach ($jpg_iter as $jpg_info) $functor($jpg_info);
