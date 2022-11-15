7e6ac0bf81c02bb5d22237527e934a2530829cdd
kurt krueckeberg <kurt.krueckeberg@gmail.com>
2022-11-15 11:01:55 -0500

    updates

--- makethb.php
+++ makethb.php
@@ -7,4 +7,4 @@ use App\GenTools\tNail as mk;
 include 'vendor/autoload.php';
 
 $m = new mk;
-$m->generate('st_cosmas_and_st_damian_church.jpg', 1280, 640, 'petzen_kirche_thb.png');
+$m->generate('st_cosmas_and_st_damian_church.jpg', 1280, 640, 'st_cosmas_and_st_damian_church_thumb.png');
